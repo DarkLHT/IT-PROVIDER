f4e522345e375b176c9b34e848f982a0dbac3ca6
hmzamth <hamidhamza18@gmail.com>
2017-11-08 22:39:00 -0500

    First Commit

---
+++ src/pages/about/about.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HomePage } from '../home/home';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
+
+  }
+
+  click() {
+    this.barcodeScanner.scan()
+      .then((result) => {
+        if (!result.cancelled) {
+          const barcodeData = new BarcodeData(result.text, result.format);
+          this.scanDetails(barcodeData);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      })
+  }
+
+
+
+
+  scanDetails(details) {
+    this.navCtrl.push(HomePage, {details: details});
+  }
+
+
+}
+
+export class BarcodeData {
+  constructor(
+    public text: String,
+    public format: String
+  ) {
+
+  }
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {BarcodeData} from '../about/about';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  barcodeData: BarcodeData;
+constructor(public navCtrl: NavController, navParams: NavParams) {
+  this.barcodeData = navParams.get('details');
+}
+
+}
